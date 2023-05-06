5ac80d05d35d901cadf2fe61ff5ea18bb57fa647
Zachary Mowatt <zamowatt@gmail.com>
2023-05-06 17:53:23 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(firstTwoDrivers){
+    return firstTwoDrivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(lastTwoDrivers){
+    return lastTwoDrivers.slice(-2);
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(ride){
+    return function(fare){
+        return fare * ride
+    };
+}
+
+const fareDoubler = function(createFareMultiplier){
+    return createFareMultiplier * 2
+}
+
+const fareTripler = function(createFareMultiplier){
+    return createFareMultiplier * 3
+}
+
+const arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function selectDifferentDrivers(arrayOfDrivers, newDrivers){
+    return newDrivers(arrayOfDrivers)
+}
