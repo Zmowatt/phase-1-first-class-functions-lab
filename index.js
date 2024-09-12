563b56bbfc143fce08da25572e262265edc4947e
Zachary Mowatt <zamowatt@gmail.com>
2024-09-11 21:12:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,32 +1,66 @@
-const returnFirstTwoDrivers = function(firstTwoDrivers){
-    return firstTwoDrivers.slice(0,2);
-}
+// const returnFirstTwoDrivers = function(firstTwoDrivers){
+//     return firstTwoDrivers.slice(0,2);
+// }
 
-const returnLastTwoDrivers = function(lastTwoDrivers){
-    return lastTwoDrivers.slice(-2);
-}
+// const returnLastTwoDrivers = function(lastTwoDrivers){
+//     return lastTwoDrivers.slice(-2);
+// }
 
-const selectingDrivers = [
-    returnFirstTwoDrivers,
-    returnLastTwoDrivers
-]
+// const selectingDrivers = [
+//     returnFirstTwoDrivers,
+//     returnLastTwoDrivers
+// ]
 
-function createFareMultiplier(ride){
-    return function(fare){
-        return fare * ride
+// function createFareMultiplier(ride){
+//     return function(fare){
+//         return fare * ride
+//     };
+// }
+
+// const fareDoubler = function(createFareMultiplier){
+//     return createFareMultiplier * 2
+// }
+
+// const fareTripler = function(createFareMultiplier){
+//     return createFareMultiplier * 3
+// }
+
+// const arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+// function selectDifferentDrivers(arrayOfDrivers, newDrivers){
+//     return newDrivers(arrayOfDrivers)
+//     }
+
+
+
+    const returnFirstTwoDrivers = function(firstTwoDrivers){
+        return firstTwoDrivers.slice(0,2);
     };
-}
+
+    const returnLastTwoDrivers = function(lastTwoDrivers){
+        return lastTwoDrivers.slice(-2);
+    };
+
+    const selectingDrivers = [
+        returnFirstTwoDrivers,
+        returnLastTwoDrivers
+];
+
+function createFareMultiplier(rate){
+    return function(fare){
+        return fare * rate
+    };
+};
 
 const fareDoubler = function(createFareMultiplier){
     return createFareMultiplier * 2
-}
+};
 
 const fareTripler = function(createFareMultiplier){
     return createFareMultiplier * 3
-}
-
-const arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+};
 
 function selectDifferentDrivers(arrayOfDrivers, newDrivers){
     return newDrivers(arrayOfDrivers)
+
 }
